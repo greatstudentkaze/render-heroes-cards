f546a77ad509360f50fb62c816e2b84713d374ea
gsk <dfyz0r@list.ru>
2020-09-17 19:57:33 +0300

    feat: add card and filter option rendering using templates, fix some bugs with movie names

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const filter = document.querySelector('.js-filter'),
   filmList = filter.querySelector('.filter__list'),
-  filterOption = filmList.querySelector('.js-filter-option'),
-  cardsList = document.querySelector('.cards__list');
+  filterOptionTemplate = filmList.querySelector('.js-filter-option'),
+  cardsList = document.querySelector('.cards__list'),
+  cardTemplate = cardsList.querySelector('.js-card');
 
 const appendPreloader = (target) => {
   const preloaderWrap = document.createElement('div'),
@@ -43,9 +44,12 @@ const addOption = ( { label, name, checked = false }, template, filterList) => {
 };
 
 const getFilms = data => {
-  const allFilms = data.reduce((allMovies, { movies }) => allMovies.concat(movies), []);
+  const allFilms = data.reduce((allMovies, { movies }) => allMovies.concat(movies), []),
+    trimmedFilms = allFilms.map(film => {
+      if (film) return film.trim();
+    });
 
-  return allFilms.filter((item, i) => item && allFilms.indexOf(item) === i);
+  return trimmedFilms.filter((item, i) => item && allFilms.indexOf(item) === i);
 };
 
 const renderFilms = films => {
@@ -53,77 +57,77 @@ const renderFilms = films => {
     label: 'All Heroes',
     name: 'all',
     checked: true
-  }, filterOption, filmList);
+  }, filterOptionTemplate, filmList);
 
   films.forEach(film => {
     addOption({
       label: film,
       name: film
-    }, filterOption, filmList);
+    }, filterOptionTemplate, filmList);
   });
 
   filterPreload.remove();
 };
 
-const renderCardYears = ({ birthDay, deathDay }) => {
-  let cardYears = '';
-  if (birthDay && deathDay) {
-    cardYears = `<p class="card__years">
-                    <span class="card__birthday">${birthDay}</span> - <span class="card__deathday">${deathDay}</span>
-                  </p>`;
-  } else if (birthDay) {
-    cardYears = `<p class="card__years">
-                    born in ${birthDay}
-                  </p>`;
-  } else if (deathDay) {
-    cardYears = `<p class="card__years">
-                    died in ${deathDay}
-                  </p>`;
-  }
-  return cardYears;
-};
+const addCard = (item, template, cardList) => {
+  const card = template.content.cloneNode(true),
+    cardTitle = card.querySelector('.js-title'),
+    cardPhoto = card.querySelector('.js-photo'),
+    cardSpecies = card.querySelector('.js-card-species'),
+    cardGender = card.querySelector('.js-card-gender'),
+    cardYears = card.querySelector('.js-years'),
+    cardBirthday = cardYears.querySelector('.js-birthday'),
+    cardDeathday = cardYears.querySelector('.js-deathday'),
+    cardStatus = card.querySelector('.js-status'),
+    cardActors = card.querySelector('.js-card-actors'),
+    cardFilms = card.querySelector('.js-films');
 
-const renderCardFilms = ({ movies }) => {
-  let cardFilms = '';
-  if (movies) {
-    movies.forEach(movie => {
+  cardTitle.textContent = item.name;
+  cardPhoto.src = item.photo ? item.photo : '';
+  cardSpecies.textContent = item.species ? item.species : '';
+  cardGender.textContent = item.gender ? item.gender : '';
+
+  if (item.birthDay && item.deathDay) {
+    cardBirthday.textContent = item.birthDay;
+    cardDeathday.textContent = item.deathDay;
+  } else if (item.birthDay) {
+    cardYears.textContent = `born in ${item.birthDay}`;
+  } else if (item.deathDay) {
+    cardYears.textContent = `died in ${item.deathDay}`;
+  } else {
+    cardYears.remove();
+  }
+
+  cardStatus.textContent = item.status ? item.status : '';
+  cardActors.textContent = item.actors;
+
+  if (item.movies) {
+    item.movies.forEach(movie => {
       const li = document.createElement('li');
       li.classList.add('card__film');
       li.textContent = movie;
-      cardFilms += `\n${li.outerHTML}`;
+
+      cardFilms.append(li);
     });
+  } else {
+    cardFilms.remove();
   }
-  return cardFilms;
+
+  cardList.append(card);
 };
 
 const renderCards = data => {
   cardsList.textContent = '';
 
   data.forEach(item => {
-    cardsList.insertAdjacentHTML('beforeend',
-      `<li class="cards__item">
-              <article class="card">
-                <h3 class="card__title">${item.name}</h3>
-                <img class="card__photo" src="${item.photo ? item.photo : ''}" alt="">
-                <div class="card__columns">
-                  <p class="card__text">${item.species ? item.species : ''}</p>
-                  <p class="card__text">${item.gender ? item.gender : ''}</p>
-                </div>
-                ${renderCardYears(item)}
-                <p class="card__status card__text">${item.status ? item.status : ''}</p>
-                <p class="card__text">Actor: ${item.actors}</p>
-                <p class="card__label">Films:</p>
-                <ul class="card__films">${renderCardFilms(item)}</ul>
-              </article>
-            </li>`);
-
+    addCard(item, cardTemplate, cardsList);
     cardsPreload.remove();
   });
 };
 
 fetch('dbHeroes.json')
   .then(response => {
-    if (response.status !== 200) throw new Error('Error');
+    if (response.status !== 200) throw new Error('Error' + response.status);
     return response.json();
   })
   .then(data => {
@@ -131,12 +135,10 @@ fetch('dbHeroes.json')
     renderFilms(films);
 
     const selectedFilms = new Set();
-
     filter.addEventListener('reset', () => {
       selectedFilms.clear();
       renderCards(data);
     });
-
     filter.addEventListener('change', evt => {
       const target = evt.target;
 
@@ -151,7 +153,7 @@ fetch('dbHeroes.json')
       else selectedFilms.delete(target.name);
 
       const selectedFilmsData = data.filter(({ movies }) => {
-        if (movies) return movies.some(movie => selectedFilms.has(movie));
+        if (movies) return movies.some(movie => selectedFilms.has(movie.trim()));
       });
       renderCards(selectedFilmsData);
     });
